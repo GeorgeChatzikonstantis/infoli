4bd9135e36b1491dcc4cbeba0f42b6de92f38e1d
George Chatzikonstantis <georgec@microlab.ntua.gr>
2017-07-11 15:09:21 +0100

    Hybrid Phi Script on Hartree: Work in Progress

--- scripts/hartree/phi/hybrid_phi_queue.sh
+++ scripts/hartree/phi/hybrid_phi_queue.sh
@@ -1,15 +1,19 @@
 #BSUB -J georgecjob
-#BSUB -o /gpfs/stfc/local/HCPhi005/ddr01/gxc30-ddr01/InfOliFull/run/output/georgecjob.out
-#BSUB -e /gpfs/stfc/local/HCPhi005/ddr01/gxc30-ddr01/InfOliFull/run/output/georgecjob.err
+#BSUB -o /gpfs/stfc/local/HCEEC005/nnm17/gxc30-nnm17/InfOliFull/run/output/georgecjob.out
+#BSUB -e /gpfs/stfc/local/HCEEC005/nnm17/gxc30-nnm17/InfOliFull/run/output/georgecjob.err
 #BSUB -R "span[ptile=1]"
 #BSUB -W 6:00
 #BSUB -n 1
 #BSUB -q phiq
 
+# number of ranks used
+ranks=2
+ranks_per_node=2
+
 # prepare Hartree's modules
 source /etc/profile.d/modules.sh
 module purge
-module load intel_mpi/5.0.3_mic intel/15.2.164_mic intel_vtune
+module load intel_mpi/5.1.3_mic intel/16.2.062_mic intel_vtune
 
 # library paths and necessary flags
 export I_MPI_FABRICS=shm:tcp
@@ -29,6 +33,10 @@ export LD_LIBRARY_PATH=/gpfs/stfc/local/apps/intel/intel_cs/2015.1.133/composer_
 /gpfs/stfc/local/apps/intel/intel_cs/2015.1.133/composer_xe_2015.1.133/tbb/lib/intel64/gcc4.4:\
 /lib:/usr/lib:/usr/X11R6/lib:/usr/local/lib:/usr/local/lib:\
 /gpfs/stfc/local/apps/intel/intel_mpi/5.0.2.044/intel64/lib:$LD_LIBRARY_PATH;
+export SINK_LD_LIBRARY_PATH=/gpfs/stfc/local/apps/intel/intel_mpi/5.1.3.181/mic/lib:$MIC_LD_LIBRARY_PATH;
+
+# root directory of InfOli dir
+rootdir=$(cd ../../../; pwd)
 
 # get name of attached MIC
 string=$HOSTNAME
@@ -36,71 +44,43 @@ MICNAME=${string/ib0/mic0}
 echo "$MICNAME"
 
 # prepare experiment: clean up the working room
-cd /gpfs/stfc/local/HCPhi005/ddr01/gxc30-ddr01/InfOliFull/run/
+cd $rootdir/run/
 rm -rf input/*
 
 # prep input: compiling executable
-cd /gpfs/stfc/local/HCPhi005/ddr01/gxc30-ddr01/InfOliFull/src
+cd $rootdir/src
 make hybrid_phi
-mv Hybrid/infoli.x /gpfs/stfc/local/HCPhi005/ddr01/gxc30-ddr01/InfOliFull/run/input
-
-# prep input: compile desired connectivity generator
-cd /gpfs/stfc/local/HCPhi005/ddr01/gxc30-ddr01/InfOliFull/tools
-make count
-mv connectivity/3d_synapse_count/conn_generator.x /gpfs/stfc/local/HCPhi005/ddr01/gxc30-ddr01/InfOliFull/run/input
+micnativeloadex Hybrid_New/infoli.x -l
+mv Hybrid_New/infoli.x $rootdir/run/input
 
 # prep input: copy runtime lib to input
-cd /gpfs/stfc/local/HCPhi005/ddr01/gxc30-ddr01/InfOliFull/run/
+cd $rootdir/run/
 cp runtime_libs/hartree/libiomp5.so input
 
 # determine analysis type for vtune
 export analysis_type="general-exploration"
 
 # preparations complete, conduct the experiment
-cd /gpfs/stfc/local/HCPhi005/ddr01/gxc30-ddr01/InfOliFull/run/input
+cd $rootdir/run/input
 for size in 10000
 do
 
-	export MYJOB="/gpfs/stfc/local/HCPhi005/ddr01/gxc30-ddr01/InfOliFull/run/input/infoli.x $size"
-
-	for synapses in 1000
+	for pct in 0.1
 	do
 
-		./conn_generator.x $((size/100)) 10 10 $synapses 1
-
-		export OMP_NUM_THREADS=200
-		export vtuneCommand="amplxe-cl -c $analysis_type -result-dir vtune_report1_$size -target-system=mic-host-launch:0 --target-install-dir=/gpfs/stfc/local/apps/intel/intel_cs/vtune_amplifier_xe_2015 --"
+		export MYJOB="$rootdir/run/input/infoli.x $size $pct 100"
+		export OMP_NUM_THREADS=$(bc -l <<< "scale=0; 200/$ranks_per_node")
+		export vtuneCommand="amplxe-cl -c $analysis_type -result-dir vtune_report1_$size \
+		-target-system=mic-host-launch:0 \
+		--target-install-dir=/gpfs/stfc/local/apps/intel/intel_cs/vtune_amplifier_xe_2015 --"
 #		$vtuneCommand mpirun -np 1 -genvall -host $MICNAME ${MYJOB}
-		/usr/bin/time -f "Total Time:\t%E\tMem Usage:\t%MkB" mpirun -np 1 -genvall -host $MICNAME ${MYJOB}
-
-		export OMP_NUM_THREADS=100
-		export vtuneCommand="amplxe-cl -c $analysis_type -result-dir vtune_report2_$size -target-system=mic-native:0 --target-install-dir=/gpfs/stfc/local/apps/intel/intel_cs/vtune_amplifier_xe_2015 --"
-#		$vtuneCommand mpirun -np 2 -genvall -host $MICNAME ${MYJOB}
-		/usr/bin/time -f "Total Time:\t%E\tMem Usage:\t%MkB" mpirun -np 2 -genvall -host $MICNAME ${MYJOB}
-
-		export OMP_NUM_THREADS=40
-		export vtuneCommand="amplxe-cl -c $analysis_type -result-dir vtune_report5_$size -target-system=mic-host-launch:0 --target-install-dir=/gpfs/stfc/local/apps/intel/intel_cs/vtune_amplifier_xe_2015 --"
-#		$vtuneCommand mpirun -np 5 -genvall -host $MICNAME ${MYJOB}
-		/usr/bin/time -f "Total Time:\t%E\tMem Usage:\t%MkB" mpirun -np 5 -genvall -host $MICNAME ${MYJOB}
-
-		export OMP_NUM_THREADS=20
-		export vtuneCommand="amplxe-cl -c $analysis_type -result-dir vtune_report10_$size -target-system=mic-host-launch:0 --target-install-dir=/gpfs/stfc/local/apps/intel/intel_cs/vtune_amplifier_xe_2015 --"
-#		$vtuneCommand mpirun -np 10 -genvall -host $MICNAME ${MYJOB}
-		/usr/bin/time -f "Total Time:\t%E\tMem Usage:\t%MkB" mpirun -np 10 -genvall -host $MICNAME ${MYJOB}
-
-		export OMP_NUM_THREADS=10
-		export vtuneCommand="amplxe-cl -c $analysis_type -result-dir vtune_report20_$size -target-system=mic-host-launch:0 --target-install-dir=/gpfs/stfc/local/apps/intel/intel_cs/vtune_amplifier_xe_2015 --"
-#		$vtuneCommand mpirun -np 20 -genvall -host $MICNAME ${MYJOB}
-		/usr/bin/time -f "Total Time:\t%E\tMem Usage:\t%MkB" mpirun -np 20 -genvall -host $MICNAME ${MYJOB}
-
-		export OMP_NUM_THREADS=4
-		export vtuneCommand="amplxe-cl -c $analysis_type -result-dir vtune_report50_$size -target-system=mic-host-launch:0 --target-install-dir=/gpfs/stfc/local/apps/intel/intel_cs/vtune_amplifier_xe_2015 --"
-#		$vtuneCommand mpirun -np 50 -genvall -host $MICNAME ${MYJOB}
-		/usr/bin/time -f "Total Time:\t%E\tMem Usage:\t%MkB" mpirun -np 50 -genvall -host $MICNAME ${MYJOB}
+#		/usr/bin/time -f "Total Time:\t%E\tMem Usage:\t%MkB" mpirun -np $ranks -genvall -host $MICNAME ${MYJOB}
+		/usr/bin/time -f "Total Time:\t%E\tMem Usage:\t%MkB" micnativeloadex $rootdir/run/input/infoli.x -a "1000 0.1 10"\
+		-e "export OMP_NUM_THREADS=200"
 
 	done
 done
 
 # the experiment is complete, clean up input and move results to output
-mv vtune_report* /gpfs/stfc/local/HCPhi005/ddr01/gxc30-ddr01/InfOliFull/run/output
-rm -rf /gpfs/stfc/local/HCPhi005/ddr01/gxc30-ddr01/InfOliFull/run/input/*
+mv vtune_report* $rootdir/run/output
+rm -rf $rootdir/run/input/*
