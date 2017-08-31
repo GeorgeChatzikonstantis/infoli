66d29da59f69b644616cbac3f46e19b9e3b032ca
Harry Sidiropoulos <harry.sidi@gmail.com>
2017-08-31 10:26:09 +0100

    Minor Bugfixes on lib paths

--- scripts/hartree_queue_jobscript.sh
+++ scripts/hartree_queue_jobscript.sh
@@ -49,18 +49,16 @@ echo "SIZE= ${SIZE}|Density= ${PROB}|Simtime= ${STIME}|Working Dir=$WORK_DIR"
 
 # prepare experiment: 
 cd $WORK_DIR
+export LD_LIBRARY_PATH=/gpfs/stfc/local/apps/intel/intel_cs/2015.1.133/composer_xe_2015.1.133/compiler/lib/mic:/gpfs/stfc/local/apps/intel/intel_cs/2015.1.133/composer_xe_2015.1.133/compiler/lib/intel64:/gpfs/stfc/local/apps/intel/intel_mpi/5.0.2.044/intel64/lib:/opt/lsf/9.1/linux2.6-glibc2.3-x86_64/lib:/gpfs/stfc/local/apps/intel/intel_mpi/4.1.3.049/intel64/lib:/gpfs/stfc/local/apps/intel/intel_cs/2015.1.133/composer_xe_2015.1.133/mpirt/lib/intel64:/gpfs/stfc/local/apps/intel/intel_cs/2015.1.133/composer_xe_2015.1.133/ipp/../compiler/lib/intel64:/gpfs/stfc/local/apps/intel/intel_cs/2015.1.133/composer_xe_2015.1.133/ipp/lib/intel64:/gpfs/stfc/local/apps/intel/intel_cs/2015.1.133/composer_xe_2015.1.133/mkl/lib/intel64:/gpfs/stfc/local/apps/intel/intel_cs/2015.1.133/composer_xe_2015.1.133/tbb/lib/intel64/gcc4.4:/lib:/usr/lib:/usr/X11R6/lib:/usr/local/lib:/usr/local/lib:/gpfs/stfc/local/systems/sja01/ljj24-sja01/testjob/intel/clck/2.2.1.003/share/mic/:/gpfs/stfc/local/apps/intel/intel_mpi/5.0.2.044/intel64/lib:$LD_LIBRARY_PATH;
+
 MYJOB="$BASE_DIR/bin/infoli.x $SIZE ${PROB} ${STIME}"
 echo $MYJOB
 
-for threads in $THREADSNUM 
-do
-    export THREADSN=$threads
-
-				ssh ${MICNAME} "export \
-				LD_LIBRARY_PATH=/gpfs/stfc/local/systems/sja01/ljj24-sja01/testjob/intel/clck/2.2.1.003/share/mic/:$LD_LIBRARY_PATH; \
-				export KMP_AFFINITY=balanced; export KMP_PLACE_THREADS=60c,4t; export OMP_NUM_THREADS=${THREADSN}; \
-				cd $WORK_DIR; ${MYJOB}"
-done
+export THREADSN=$THREADSNUM 
+ssh ${MICNAME} "export \
+    LD_LIBRARY_PATH=$LD_LIBRARY_PATH; \
+    export KMP_AFFINITY=balanced; export KMP_PLACE_THREADS=60c,4t; export OMP_NUM_THREADS=${THREADSN}; \
+    cd $WORK_DIR; ${MYJOB}"
 
 echo "Simulation Finished, results in \"$WORK_DIR\" folder.\n"
 echo "-----------------------------------\n"
